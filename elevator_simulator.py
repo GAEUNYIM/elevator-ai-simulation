c3892be90eafb383fed55e90d47bd4efdd490b33
GAEUNYIM <rkdms4031@unist.ac.kr>
2022-10-12 22:36:14 +0900

    basic ui implemented

--- elevator_simulator.py
+++ elevator_simulator.py
@@ -18,7 +18,6 @@ class Direction(Enum):
     STAY = 0
     DOWN = -1
 
-    
 Point = namedtuple('Point', 'x, y')
 
 # rgb colors
@@ -28,9 +27,9 @@ BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 BLACK = (0,0,0)
 
-ELEVATOR_WIDTH = 40 
-ELEVATOR_HEIGHT = 80
-BLOCK_SIZE = 20
+ELEVATOR_WIDTH = 100
+ELEVATOR_HEIGHT = 200
+# BLOCK_SIZE = 20
 SPEED = 20
 
 class EGCS:
@@ -42,25 +41,31 @@ class EGCS:
         self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('Elevator Group Control System')
         self.clock = pygame.time.Clock()
+        self.ticks = pygame.time.get_ticks()
         
         # init game state
         self.direction = Direction.STAY
         
-        self.head = Point(self.w/2, self.h/2)
-        self.snake = [self.head, 
-                      Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        # self.head = Point(self.w/2, self.h/2)
+        # self.snake = [self.head, 
+        #               Point(self.head.x-BLOCK_SIZE, self.head.y),
+        #               Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
         
+        self.elevators = [1, 3] # List : Contains the location of elevators. Double-deck Elevator.
+        self.hall_calls = [3, 0, 1, 2] # List : Contains the flag of hall calls. Binary
         self.score = 0
         self.passenger = None
         self._place_passenger()
     
     def _place_passenger(self):
-        x = 1
-        y = random.randint(0, 8)*BLOCK_SIZE
-        self.passenger = Point(x, y)
+        # floor = random.randint(0,4)
+        # self.hall_calls[floor] += 1
+        # x = 1
+        # y = random.randint(0, 8)*ELEVATOR_HEIGHT
+        # self.passenger = Point(x, y)
         # If passenger meets elevator, then 
             # allocate new passenger next time
+        pass
         
     def play_step(self):
         # 1. collect user input
@@ -68,78 +73,108 @@ class EGCS:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.direction = Direction.LEFT
-                elif event.key == pygame.K_RIGHT:
-                    self.direction = Direction.RIGHT
-                elif event.key == pygame.K_UP:
-                    self.direction = Direction.UP
-                elif event.key == pygame.K_DOWN:
-                    self.direction = Direction.DOWN
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_LEFT:
+            #         self.direction = Direction.LEFT
+            #     elif event.key == pygame.K_RIGHT:
+            #         self.direction = Direction.RIGHT
+            #     elif event.key == pygame.K_UP:
+            #         self.direction = Direction.UP
+            #     elif event.key == pygame.K_DOWN:
+            #         self.direction = Direction.DOWN
         
         # 2. move
-        self._move(self.direction) # update the head
-        self.snake.insert(0, self.head)
+        # self._move(self.direction) # update the head
+        # self.snake.insert(0, self.head)
         
         # 3. check if game over
-        game_over = False
-        if self._is_collision():
-            game_over = True
-            return game_over, self.score
+        # game_over = False
+        # if self._is_collision():
+        #     game_over = True
+        #     return game_over, self.score
             
         # 4. place new food or just move
-        if self.head == self.food:
-            self.score += 1
-            self._place_food()
-        else:
-            self.snake.pop()
+        # if self.head == self.food:
+        #     self.score += 1
+        #     self._place_food()
+        # else:
+        #     self.snake.pop()
         
         # 5. update ui and clock
         self._update_ui()
         self.clock.tick(SPEED)
         # 6. return game over and score
-        return game_over, self.score
+        # return game_over, self.score
+
+        return False, 0
     
     def _is_collision(self):
-        # hits boundary
-        if self.head.x > self.w - BLOCK_SIZE or self.head.x < 0 or self.head.y > self.h - BLOCK_SIZE or self.head.y < 0:
-            return True
-        # hits itself
-        if self.head in self.snake[1:]:
-            return True
+        # # hits boundary
+        # if self.head.x > self.w - BLOCK_SIZE or self.head.x < 0 or self.head.y > self.h - BLOCK_SIZE or self.head.y < 0:
+        #     return True
+        # # hits itself
+        # if self.head in self.snake[1:]:
+        #     return True
         
-        return False
+        # return False
+        pass
+
+    def _floor_to_point_agent(self, agent, floor):
+        if agent == 0:
+            x_align = 0
+        elif agent == 1:
+            x_align = 300
+
+        return Point(x_align, 800-(floor*ELEVATOR_HEIGHT))
+
+    def _floor_to_point_passenger(self, floor):
+        x_align = 180
+
+        return Point(x_align, 770-(floor*ELEVATOR_HEIGHT))
+
         
     def _update_ui(self):
-        self.display.fill(BLACK)
+        self.display.fill(WHITE)
         
-        for pt in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
-            
-        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        # for pt in self.snake:
+        #     pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+        #     pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+        for agt, flr in enumerate(self.elevators):
+            pt = self._floor_to_point_agent(agt, flr)
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, ELEVATOR_WIDTH, ELEVATOR_HEIGHT))
+
+        for flr, psg in enumerate(self.hall_calls):
+            pt = self._floor_to_point_passenger(flr)
+            text = font.render(str(psg), True, BLACK)
+            self.display.blit(text, [pt.x, pt.y])
+
+        # pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
         
-        text = font.render("Score: " + str(self.score), True, WHITE)
+        text = font.render("Score: " + str(self.score), True, BLACK)
+        # time = font.render("TIME: " + str(self.clock), True, BLACK )
         self.display.blit(text, [0, 0])
+        # self.display.blit(time, [0, 40])
         pygame.display.flip()
+        pass
         
     def _move(self, direction):
-        x = self.head.x
-        y = self.head.y
-        if direction == Direction.RIGHT:
-            x += BLOCK_SIZE
-        elif direction == Direction.LEFT:
-            x -= BLOCK_SIZE
-        elif direction == Direction.DOWN:
-            y += BLOCK_SIZE
-        elif direction == Direction.UP: 
-            y -= BLOCK_SIZE
+        # x = self.head.x
+        # y = self.head.y
+        # if direction == Direction.RIGHT:
+        #     x += BLOCK_SIZE
+        # elif direction == Direction.LEFT:
+        #     x -= BLOCK_SIZE
+        # elif direction == Direction.DOWN:
+        #     y += BLOCK_SIZE
+        # elif direction == Direction.UP: 
+        #     y -= BLOCK_SIZE
             
-        self.head = Point(x, y)
+        # self.head = Point(x, y)
+        pass
             
 
 if __name__ == '__main__':
+    
     game = EGCS()
     
     # game loop
@@ -150,6 +185,5 @@ if __name__ == '__main__':
             break
         
     print('Final Score', score)
-        
         
     pygame.quit()
---
+++ game.py
@@ -0,0 +1,7 @@
+import pygame
+
+clock = pygame.time.Clock()
+
+while(pygame.time.delay(200)):
+    trial = pygame.time.get_ticks()
+    print(trial)
