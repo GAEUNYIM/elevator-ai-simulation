0deebdb29e00799d723f1152ae22857d47929aae
GAEUNYIM <rkdms4031@unist.ac.kr>
2022-11-15 20:30:44 +0900

    assigning

--- elevator_simulator.py
+++ elevator_simulator.py
@@ -164,6 +164,7 @@ class EGCS:
             self.state_hall_calls[rand_flr-1] += 1
             self.flag_hall_calls[rand_flr-1] = 1
             self.temp_ticks = curr_ticks
+        # self._assign_highest_first()
         self._assign()
     
     def _is_collision(self):
@@ -177,6 +178,23 @@ class EGCS:
         # return False
         pass
 
+    def _assign_flr_to_elv(self, elvid, flr):
+        if elvid == 1:
+            self.queue_elv1.append(flr)
+            self.queue_elv1.sort(reverse=True)
+            for f in self.queue_elv1:
+                if f > self.state_elevators[0]:
+                    self.queue_elv1.remove(f)
+                    self.queue_elv1.append(f)
+        elif elvid == 2:
+            self.queue_elv2.append(flr)
+            self.queue_elv2.sort(reverse=True)
+            for f in self.queue_elv2:
+                if f > self.state_elevators[0]:
+                    self.queue_elv2.remove(f)
+                    self.queue_elv2.append(f)
+        else:
+            print("WATCH OUT")
 
     def _assign_flr_to_sys(self, elv, flr):
         '''
@@ -190,25 +208,132 @@ class EGCS:
             if flr not in self.queue_elv1:
                 self.queue_elv1.append(flr)
                 self.queue_elv1.sort()
-                for flr in self.queue_elv1:
-                    if flr > self.state_elevators[0]:
-                        self.queue_elv1.remove(flr)
-                        self.queue_elv1.append(flr)
+                for f in self.queue_elv1:
+                    if f > self.state_elevators[0]:
+                        self.queue_elv1.remove(f)
+                        self.queue_elv1.append(f)
                 # self.flag_hall_calls[flr-1] = 0
         elif elv == 2:
             if flr not in self.queue_elv2:
                 self.queue_elv2.append(flr)
                 self.queue_elv2.sort()
-                for flr in self.queue_elv2:
-                    if flr > self.state_elevators[0]:
-                        self.queue_elv2.remove(flr)
-                        self.queue_elv2.append(flr)
+                for f in self.queue_elv2:
+                    if f > self.state_elevators[1]:
+                        self.queue_elv2.remove(f)
+                        self.queue_elv2.append(f)
                 # self.flag_hall_calls[flr-1] = 0
         else:
             print("WATCH OUT")
 
-    
     def _assign(self):
+        '''
+        1. Select flr to visit next.
+        2. Assign appropriate elv to visit there.
+
+        '''
+        # self._assign_next_visit_flr()
+        self._assign_highest_first()
+
+        print("===== information =====")
+        print("self.state_hall_calls : ", self.state_hall_calls)
+        print("self.flag_hall_calls : ", self.flag_hall_calls)
+        print("self.state_elevators : ", self.state_elevators)
+        print("self.state_directions : ", self.state_directions)
+
+        print("get highest hall call flr: ", self._get_highest_hall_call_flr())
+        print("queue_elv1 : ", self.queue_elv1)
+        print("queue_elv2 : ", self.queue_elv2)
+        
+        print("Total arrived passengers : ", self.arrived_passengers)
+
+    def _assign_next_visit_flr(self):
+        '''_assign_flr_to_elv
+        Return the floor should be visit next. Update every ticks.
+        '''
+        # Should be designed in detail..!!
+
+        state_elv1 = self.state_elevators[0]
+        state_elv2 = self.state_elevators[1]
+        # Case 1. Both elevators are on the GF.
+        if state_elv1 == 1 and state_elv2 == 1:
+            next_visit_flr = self._get_highest_hall_call_flr()
+            self._assign_flr_to_sys(1, next_visit_flr)
+
+        # Case 2. One of the elevators is on the GF, one is moving.
+        # Case 2-1. Elv2 is moving.
+        elif state_elv1 == 1 and state_elv2 != 1: 
+
+            # Case 2-1-1. Check whether there is request below elv2.
+            next_visit_flr_below = self._return_request_below_flr(state_elv2)
+
+            # If there exists request below elv2, then push it into a queue.
+            if next_visit_flr_below != 0:
+                next_visit_flr = next_visit_flr_below
+                closer_elvid = self._get_closest_elevator_with_flr(next_visit_flr)
+                self._assign_flr_to_elv(closer_elvid, next_visit_flr)
+            # If there are no request below elv2, then go to the 1st flr
+            # else:
+            #     next_visit_flr = 1
+            #     self._assign_flr_to_elv(2, next_visit_flr)
+
+            # Case 2-1-2. Check whether there is request above elv2.
+            next_visit_flr_above = self._return_request_above_flr(state_elv2)
+
+            # If there exists request above elv2, then assign it to elv1.
+            if next_visit_flr_above != 0:
+                next_visit_flr = next_visit_flr_below
+                self._assign_flr_to_elv(1, next_visit_flr)
+            
+        
+        # Case 2-2. Elv1 is moving.
+        elif state_elv1 != 1 and state_elv2 == 1: 
+
+            # Case 2-2-1. Check whether there is request below elv1. 
+            next_visit_flr_below = self._return_request_below_flr(state_elv1)
+
+            # If there exists request below elv1, then push it into a queue.
+            if next_visit_flr_below != 0:
+                next_visit_flr = next_visit_flr_below
+                closer_elvid = self._get_closest_elevator_with_flr(next_visit_flr)
+                self._assign_flr_to_elv(closer_elvid, next_visit_flr)
+            # If there are no request below elv2, then go to the 1st flr
+            # else:
+            #     next_visit_flr = 1
+            #     self._assign_flr_to_elv(1, next_visit_flr)
+
+            # Case 2-2-2. Check whether there is request above elv1.
+            next_visit_flr_above = self._return_request_above_flr(state_elv1)
+
+            # If there exists request above elv1, then assign it to elv2.
+            if next_visit_flr_above != 0:
+                next_visit_flr = next_visit_flr_below
+                self._assign_flr_to_elv(2, next_visit_flr)
+
+            
+        # Case 3. Both elevators are moving.
+        elif state_elv1 != 1 and state_elv2 != 1:
+
+            # Case 3-1. both elevators are on the same floor.
+            if state_elv1 == state_elv2:
+                next_visit_flr_below = self._return_request_below_flr(state_elv1)
+                self._assign_flr_to_elv(1, next_visit_flr)
+
+            # Case 3-2. two are on the different floor.
+            elif state_elv1 > state_elv2:
+                next_visit_flr_below = self._return_request_below_flr(state_elv1)
+                if next_visit_flr_below > state_elv2:
+                    self._assign_flr_to_elv(1, next_visit_flr)
+                elif next_visit_flr_below < state_elv2:
+                    self._assign_flr_to_elv(2, next_visit_flr)
+            
+            elif state_elv1 < state_elv2:
+                next_visit_flr_below = self._return_request_below_flr(state_elv2)
+                if next_visit_flr_below > state_elv1:
+                    self._assign_flr_to_elv(2, next_visit_flr)
+                elif next_visit_flr_below < state_elv1:
+                    self._assign_flr_to_elv(1, next_visit_flr)
+
+    def _assign_highest_first(self):
         '''
         If there exist request under elv1 or elv2, push it into the queue.
         While it goes to request, if there comes new request which has high priority,
@@ -245,22 +370,9 @@ class EGCS:
             for i in range(FLOORS - highest_elv_flr, FLOORS, -1):
                 if self.flag_hall_calls[i] == 1:
                     # Assign it
-                    pass
-                    # Pop it
-        
-        print("===== information =====")
-        # print("get next visit flr : ", self._get_next_visit_flr())
-        print("get highest hall call flr: ", self._get_highest_hall_call_flr())
-        print("highest elevator flr: ", self._get_closest_elevator_with_flr(9))
-        print("get highest elvid, flr: ", self._get_highest_elevator_flr_info(9))
-        print("get highest agent flr: ", self._get_highest_agent_flr())
-        print("queue_elv1 : ", self.queue_elv1)
-        print("queue_elv2 : ", self.queue_elv2)
-        print("state_elevators : ", self.state_elevators)
-        print("Total arrived passengers : ", self.arrived_passengers)
-        print("self.state_hall_calls : ", self.state_hall_calls)
-        print("self.flag_hall_calls : ", self.flag_hall_calls)
-        print("self.state_directions : ", self.state_directions)
+                    elvid = self._get_closest_elevator_with_flr(i)
+                    self._assign_flr_to_elv(elvid, i)
+                    
 
     def _move(self):
         '''
@@ -270,6 +382,7 @@ class EGCS:
         if len(self.queue_elv1) != 0:
             next_visit_flr = self.queue_elv1[0]
             self.queue_elv1.remove(next_visit_flr)
+            self.flag_hall_calls[next_visit_flr-1] = 0
         
             # Check whether the candidate flr is on direction of elv1
             if self._check_flr_on_direction(1, next_visit_flr) == 1:
@@ -282,6 +395,7 @@ class EGCS:
         if len(self.queue_elv2) != 0:
             next_visit_flr = self.queue_elv2[0]
             self.queue_elv2.remove(next_visit_flr)
+            self.flag_hall_calls[next_visit_flr-1] = 0
         
             # Check whether the candidate flr is on direction of elv1
             if self._check_flr_on_direction(2, next_visit_flr) == 1:
@@ -399,30 +513,27 @@ class EGCS:
         # Return the highest 
         return max_elvid, max_flr
 
+    def _return_request_above_flr(self, flr):
+        '''
+        Check whether there are request above the target floor.
+        '''
+        flag = 0
+        for f in range(FLOORS-1, flr, -1):
+            if self.state_hall_calls[f] == 1:
+                flag = f
+        return flag
+
+    def _return_request_below_flr(self, flr):
+        '''
+        Check whether there are request below the target floor.
+        '''
+        flag = 0
+        for f in range(flr-1, 1, -1):
+            if self.state_hall_calls[f] == 1:
+                flag = f
+        return flag
 
 
-    # def _get_next_visit_flr(self):
-    #     '''
-    #     Return the floor should be visit next. Update every ticks.
-    #     '''
-    #     # Should be designed in detail..!!
-
-    #     # Only consider the floors belgit ow both elevator.
-    #     highest_elv_flr = self._get_highest_elevator_flr_info(9)[1]
-
-    #     # Where both elevator are on the GF
-    #     if highest_elv_flr == 1: 
-    #         next_visit_flr = self._get_highest_hall_call_flr()
-        
-    #     # While examining the hall calls lower than the highest elevator,
-    #     # if there is hall call, then assign it to the closests elev
-    #     for i in range(FLOORS - highest_elv_flr, FLOORS, -1):
-    #         if self.state_hall_calls[i] == 1:
-    #             # Assign it
-    #             pass
-    #             # Pop it
-
-    #     return next_visit_flr
 
 
     def _get_highest_hall_call_flr(self):
